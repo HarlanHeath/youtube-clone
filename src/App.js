ba3704b8c288e421b9c550f0d371bf01eb625fcb
User <user@Users-MacBook-Pro.local>
2019-08-07 12:58:01 -0500

    setup search field

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
 import React from 'react';
-import youtube from './api/youtube'
-import { SearchBar, VideoList, VideoDetail } from "./Components"
 
+import youtube from './api/youtube'
+
+import SearchBar from "./Components/SearchBar"
+import VideoList from "./Components/VideoList"
+import VideoDetail from "./Components/VideoDetail"
 
 import { Grid } from '@material-ui/core';
 
 
+
+const handleSubmit = async (searchTerm) => {
+  const response = await youtube.get('search', {
+    params: {
+      part: 'snippet',
+      maxResults: 5,
+      key: " "/* past my api key here */,
+      q: searchTerm
+    }
+  })
+}
+
+
 function App() {
+
+
   return (
-    <Grid justify="center" container spacing={16}>
+    <Grid justify="center" container spacing={10}>
       <Grid item xs={12}>
-        <Grid container spacing={16}>
+        <Grid container spacing={10}>
           <Grid item xs={12}>
-            <SearchBar />
+            <SearchBar onFormSubmit={handleSubmit} />
           </Grid>
           <Grid item xs={8}>
             <VideoDetail />
